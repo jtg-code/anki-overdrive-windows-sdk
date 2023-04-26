ef83ce1189a123d9ca210775f212f274aa0d6d6c
Jul Go <83109142+jtg-code@users.noreply.github.com>
2023-04-26 18:47:03 +0200

    Uploaded new version
    Uploaded new version 0.5.1
    - added track notify
    - added ping
    - removed old not used function
    - improved performance

---
+++ anki_sdk/exceptions.py
@@ -0,0 +1,15 @@
+class NotFoundError(Exception):
+    """
+    Reason: Reason why the car could not be found
+    """
+    def __init__(self, reason: str = "Car not found"):
+        self.reason = reason
+        super().__init__(self.reason)
+        
+class ImportDisabled(Exception):
+    """
+    Reason: Reason why the import disabled is
+    """
+    def __init__(self, reason: str = "Import disabled"):
+        self.reason = reason
+        super().__init__(self.reason)
