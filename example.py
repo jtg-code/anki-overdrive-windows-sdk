88c6b30c897fbf60682b74782f2e115e7732ef5e
Jul Go <83109142+jtg-code@users.noreply.github.com>
2023-05-25 19:23:41 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,32 +1,51 @@
-import anki_sdk.cars as cars
-import anki_sdk.controller as controller
-import anki_sdk.utils as utils
+import anki_sdk as sdk
+import threading
+import time
 
+def controll_car(address: str):
+    car = sdk.cars.CarClass(address)
+    car.connect()
+    car.start_notify()
+    controller = sdk.controller.ControllerClass(car)
+    print(f"Pong! {car.ping()}ms")
+    controller.set_speed(500, 500)
+    time.sleep(10)
+    controller.left_lane(1.0)
+    time.sleep(10)
+    controller.right_lane(1.0)
+    time.sleep(10)
+    
+    
+    @car.notifycallback(sdk.cars.Receive.Track.TRACK_CHANGE)
+    def new_track(sender, data):
+        print("New Track!")
+        
+    @car.notifycallback(sdk.cars.Receive.Track.FINISHLINE)
+    def finish_line(sender, data):
+        print("Finish!")
+        
+    @car.notifycallback(sdk.cars.Receive.Track.SPECIAL_TRACK)
+    def special_track(sender, data):
+        print("Special Track!")
+        
+    @car.notifycallback(sdk.cars.Receive.Track.STRAIGHT_TRACK)
+    def straight_track(sender, data):
+        print("New Straight Track!")
+        
+    @car.notifycallback(sdk.cars.Receive.Connection.PING_RESPONSE)
+    def ping_response(sender, data):
+        print("Ping Response!")
+    
+    car.stop_notify()
+    car.disconnect()
+    
 def main():
-    myCar: cars.carClass = cars.carClass("XX:XX:XX:XX")
-    myController: controller.controllerClass = controller.controllerClass(myCar)
-    speed, accel = 500, 1000
-    myController.setSpeed(speed, accel)
+    car_list_address = sdk.utils.scanner(False)
+    car_list = {address: sdk.utils.get_data(address) for address in car_list_address}
+    for car in car_list:
+        threading.Thread(target=controll_car, args=[car])
 
-    lanes = 3
-    myController.leftLane(lanes)
-    myController.rightLane(lanes)
 
-    rightLane = 44.5
-    leftLane = -44.5
-    myController.changeLane(rightLane)
-    myController.changeLane(leftLane)
-
-    @myCar.notifyCallback(type=cars.Receive.trackChange)
-    def newTrack(sender, data):
-        print("New track")
-        
-        
-    @myCar.notifyCallback(type=cars.Receive.specialTrack)
-    def specialTrack(sender, data):
-        print("Special track")
-        
-    print(myCar.ping())
 
 if __name__ == "__main__":
     main()
